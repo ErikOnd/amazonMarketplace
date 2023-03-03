c4d28c05f4e168b24f4fc737b07f71032b1e1e43
Erik <erik.ondra@yahoo.com>
2023-03-03 15:24:57 +0100

    handle errror for image upload

--- src/api/files/index.js
+++ src/api/files/index.js
@@ -2,6 +2,7 @@ import Express from "express";
 import multer from "multer";
 import { extname } from "path";
 import { saveProductsImg } from "../../lib/fs-tools.js";
+import createHttpError from "http-errors";
 
 const filesRouter = Express.Router();
 
@@ -11,11 +12,15 @@ filesRouter.post(
   async (req, res, next) => {
     try {
       console.log(req.file);
-      const originalFileExtension = extname(req.file.originalname);
-      console.log(originalFileExtension);
-      const fileName = req.params.productID + originalFileExtension;
-      await saveProductsImg(fileName, req.file.buffer);
-      res.send({ message: "file uploaded" });
+      if (req.file !== undefined) {
+        const originalFileExtension = extname(req.file.originalname);
+        console.log(originalFileExtension);
+        const fileName = req.params.productID + originalFileExtension;
+        await saveProductsImg(fileName, req.file.buffer);
+        res.send({ message: "file uploaded" });
+      } else {
+        next(createHttpError(404, `The uploaded image is undefined`));
+      }
     } catch (error) {
       next(error);
     }
